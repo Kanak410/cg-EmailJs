c4162f7f6227ccb1145edd673147d3dbc92124d6
Kanak Rajput <rajputkanak078@gmail.com>
2025-03-06 02:59:44 +0530

    [kanak]Add. Added regex to validate email

---
+++ emailChecker.js
@@ -0,0 +1,18 @@
+function validateEmail(email){
+    let emailRegex=/^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@bridgelabz(\.[a-zA-Z]{2,}){1,2}$/;
+    if (emailRegex.test(email)){
+        console.log(email+"is Vaild Email");
+    }else{
+        console.log(email+"is not vaild email");
+    }
+}
+validateEmail("abc@bridgelabz.co");
+validateEmail("abc.xyz@bridgelabz.co.in");
+validateEmail("abc@bridgelabz.com");
+
+validateEmail("abc@.co.in");
+validateEmail("abc.xyz@bridgelabz");
+validateEmail("abc@bridgelabz.c");
+validateEmail("abc..xyz@bridgelabz.co.in");
+validateEmail("abc@bridgelabz..co.in");
+validateEmail("abc@bridgelabz.c@o.in");
